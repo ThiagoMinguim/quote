e4dd6d71edcfa97c87bc72ba4dd8e28ca131d8b7
Thiago Minguim <Chazy249@gmail.com>
2022-07-06 02:12:20 -0300

    configure app settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+import Home from '@/pages/Home'
+import GlobalStyle from './styles/global'
+import theme from './styles/theme'
+
 function App() {
   return (
-    <div className="App">
-      <h1>olá</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Home />
+    </ThemeProvider>
   )
 }
 
