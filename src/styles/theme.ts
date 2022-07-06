a80d3575aaa9ec94ba2b6137da24205325c260f4
Thiago Minguim <Chazy249@gmail.com>
2022-07-06 02:14:43 -0300

    theme settings

---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+const theme = {
+  background: '#FFFFFF',
+  colors: {
+    box: '#333333',
+    border: '#F7DF94'
+  },
+  text: {
+    primary: '#333333',
+    secondary: '#4F4F4F',
+    footer: '#828282'
+  }
+}
+
+export default theme
