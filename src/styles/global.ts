5e9b55da79a5fe59bc474481dac5e13e0674ae84
Thiago Minguim <Chazy249@gmail.com>
2022-07-06 02:13:55 -0300

    global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,7 +4,9 @@ const GlobalStyle = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
-    box-sizing: border-box
+    box-sizing: border-box;
+    /* width: 100%; */
+    /* height: 100%; */
   }
 
   html{
@@ -18,6 +20,7 @@ const GlobalStyle = createGlobalStyle`
   body{
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
+
 
 `
 
