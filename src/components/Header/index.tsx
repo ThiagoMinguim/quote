cb35c335aeac7db31832862f06d2a1a14eee6fcb
Thiago Minguim <Chazy249@gmail.com>
2022-07-06 14:14:22 -0300

    header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,7 @@
 import { Container } from './styles'
 import { MdOutlineAutorenew } from 'react-icons/md'
-import { useTheme } from 'styled-components'
 
 const Header = () => {
-  const theme = useTheme()
-
   return (
     <Container>
       <span>random</span>
