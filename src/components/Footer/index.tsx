2408a2c6953ba483733552e3d957a3d3c502b562
Thiago Minguim <Chazy249@gmail.com>
2022-07-06 02:13:23 -0300

    Footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import { Container } from './style'
+
+const Footer = () => {
+  return (
+    <Container>
+      <span>
+        Created by <strong>Thiago Minguim</strong> - devChallanger.io
+      </span>
+    </Container>
+  )
+}
+
+export default Footer
---
+++ src/components/Footer/style.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  width: 100%;
+
+  justify-content: center;
+  align-items: flex-end;
+
+  margin: 2.4rem 0;
+
+  position: absolute;
+  bottom: 0;
+
+  span {
+    font-weight: 700;
+    font-size: 1.4rem;
+    color: ${({ theme }) => theme.text.footer};
+  }
+
+  strong {
+    text-decoration: underline;
+  }
+`
