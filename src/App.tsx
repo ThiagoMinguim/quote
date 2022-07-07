078b434d8e063c3f20498b33da4d47f544df3aea
Thiago Minguim <Chazy249@gmail.com>
2022-07-06 23:33:09 -0300

    att app

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { ThemeProvider } from 'styled-components'
 import Home from '@/pages/Home'
 import GlobalStyle from './styles/global'
 import theme from './styles/theme'
+import Author from './pages/Author'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Home />
+      {/* <Home /> */}
+      <Author />
+      <Footer />
     </ThemeProvider>
   )
 }
