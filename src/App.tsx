bc5aefbf8e23b99cf620fc460b0cc66e23809dbe
Thiago Minguim <Chazy249@gmail.com>
2022-07-06 23:49:14 -0300

    adjusts routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import Home from '@/pages/Home'
 import GlobalStyle from './styles/global'
@@ -9,8 +10,12 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      {/* <Home /> */}
-      <Author />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="author" element={<Author />} />
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </ThemeProvider>
   )
