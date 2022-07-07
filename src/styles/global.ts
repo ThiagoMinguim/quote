29c2c0f8ec7bf0c308c6e8a2deb9ec3a29ad18c7
Thiago Minguim <Chazy249@gmail.com>
2022-07-06 23:31:24 -0300

    change global style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,20 +5,25 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    /* width: 100%; */
-    /* height: 100%; */
+    
   }
 
   html{
     font-size: 62.5%;
-  }
-
-  html,body, #__next{
     height: 100%;
   }
 
+  /* html,body, #root{
+    min-height: 100vh;
+  } */
+
   body{
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
+    min-height: 100%;
+    position: relative;
+    
+    padding-bottom: 6rem;
   }
 
 
