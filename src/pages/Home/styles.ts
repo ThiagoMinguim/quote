e97c9e31e95768dcd9c6d8c7bd152ba103dc477d
Thiago Minguim <Chazy249@gmail.com>
2022-07-07 00:13:30 -0300

    work in progress

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -8,6 +8,16 @@ export const Wrapper = styled.div`
   /* background-color: red; */
   /* justify-content: space-between; */
   padding-bottom: 2.5rem;
+
+  button {
+    background: none;
+    color: inherit;
+    border: none;
+    padding: 0;
+    font: inherit;
+    cursor: pointer;
+    outline: inherit;
+  }
 `
 
 export const Content = styled.div`
@@ -18,4 +28,7 @@ export const Content = styled.div`
   /* width: 80%;   */
 
   gap: 10rem;
+`
+export const Red = styled.div`
+  background-color: red;
 `
