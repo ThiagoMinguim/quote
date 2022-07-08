f848db2ad127dc0015b211f60b0f4ea2d4093678
Thiago Minguim <Chazy249@gmail.com>
2022-07-08 17:15:29 -0300

    work in progress

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,27 @@
 import { Container } from './styles'
 import { MdOutlineAutorenew } from 'react-icons/md'
-import { useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { useCallback, useEffect, useState } from 'react'
+import { useQuote } from '@/hooks/quote-provider'
 
 const Header = () => {
+  const { getRandomQuote, quote } = useQuote()
+  const location = useLocation()
   const navigate = useNavigate()
 
-  function handleClick() {
-    navigate('/')
-  }
+  const handleFetchQuote = useCallback(async () => {
+    await getRandomQuote()
+
+    if (location.pathname !== '/') {
+      navigate('/')
+    }
+
+    console.log(quote)
+  }, [getRandomQuote, location])
 
   return (
     <Container>
-      <button onClick={handleClick}>
+      <button onClick={handleFetchQuote}>
         <span>random</span>
         <MdOutlineAutorenew />
       </button>
