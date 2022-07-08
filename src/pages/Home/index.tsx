ee1251fb8d512acb19b32475e941904d131bb777
Thiago Minguim <Chazy249@gmail.com>
2022-07-08 17:15:39 -0300

    work in progress

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,14 +2,26 @@ import RandomBox from '@/components/RandomBox'
 import Header from '@/components/Header'
 import Phrase from '@/components/Phrase'
 import { Wrapper, Content } from './styles'
+import { SingleQuote, useQuote } from '@/hooks/quote-provider'
+import { useEffect } from 'react'
 
-const Home = () => {
+type HomeProps = {
+  quote: SingleQuote
+}
+
+const Home = ({ quote }: HomeProps) => {
+  const { setQuote } = useQuote()
+
+  useEffect(() => {
+    setQuote(quote)
+  }, [quote, setQuote])
+
   return (
     <>
       <Wrapper>
         <Header />
         <Content>
-          <Phrase />
+          <Phrase text={quote.quoteAuthor} />
           <RandomBox />
         </Content>
       </Wrapper>
