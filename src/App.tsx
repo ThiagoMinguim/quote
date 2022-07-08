725f844d5b1ff3c504aa6659f17265cbbca03b00
Thiago Minguim <Chazy249@gmail.com>
2022-07-08 17:15:23 -0300

    work in progress

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,19 @@ import GlobalStyle from './styles/global'
 import theme from './styles/theme'
 import Author from './pages/Author'
 import Footer from './components/Footer'
+import { QuoteProvider } from './hooks/quote-provider'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="author" element={<Author />} />
-        </Routes>
+        <QuoteProvider>
+          <Routes>
+            <Route index element={<Home quote={} />} />
+            <Route path="author" element={<Author />} />
+          </Routes>
+        </QuoteProvider>
       </BrowserRouter>
       <Footer />
     </ThemeProvider>
