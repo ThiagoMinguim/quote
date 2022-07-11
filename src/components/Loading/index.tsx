9802b3b2bb0a7156a90f3ebd5eaeb3a54d571e5a
Thiago Minguim <Chazy249@gmail.com>
2022-07-11 15:56:58 -0300

    load component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,24 @@
+import Lottie from 'react-lottie'
+import load_app from '../../assets/load_app.json'
+import { Container } from './styles'
+
+const Loading = () => {
+  return (
+    <Container>
+      <Lottie
+        options={{
+          loop: true,
+          autoplay: true,
+          animationData: load_app,
+          rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice'
+          }
+        }}
+        height={150}
+        width={150}
+      />
+    </Container>
+  )
+}
+
+export default Loading
