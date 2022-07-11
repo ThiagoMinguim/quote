8274457e9dc85dfe17ca1cdef41ba7efee102ec8
Thiago Minguim <Chazy249@gmail.com>
2022-07-11 14:02:52 -0300

    work in progress

--- src/pages/Author/index.tsx
+++ src/pages/Author/index.tsx
@@ -1,22 +1,28 @@
 import Header from '@/components/Header'
 import Phrase from '@/components/Phrase'
 import { QuoteContext } from '@/hooks/quote-provider'
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 import { Wrapper, NameAuthor } from './styles'
 
 const Author = () => {
-  const { quote } = useContext(QuoteContext)
+  const { authorQuotes, getAuthorQuotes } = useContext(QuoteContext)
+  const location = useLocation()
 
+  useEffect(() => {
+    const authorName = new URLSearchParams(location.search).get('name')
+    getAuthorQuotes(authorName!)
+  }, [])
   return (
     <>
       <Wrapper>
         <Header />
 
-        <NameAuthor>{quote?.quoteAuthor}</NameAuthor>
+        <NameAuthor>{authorQuotes[0]?.quoteAuthor}</NameAuthor>
 
-        <Phrase />
-        <Phrase />
-        <Phrase />
+        {authorQuotes?.map(quote => (
+          <Phrase key={quote._id} quote={quote} />
+        ))}
       </Wrapper>
     </>
   )
